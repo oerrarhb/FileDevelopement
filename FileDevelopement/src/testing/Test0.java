f8b76e80ab8bbcc40a74dc6d54ea65889b958432
Jhon Othmane ERRARHBI <othmane.errarhbi@gmail.com>
2019-04-25 13:15:15 +0200

    Third Commit Solution of copying from file to another

--- FileDevelopement/src/testing/Test0.java
+++ FileDevelopement/src/testing/Test0.java
@@ -1,9 +1,39 @@
 package testing;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Test0 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Solution of copying from file to another
+		
+		
+		String path1 = "C:\\Users\\Lenovo\\Desktop\\test1.txt";
+		String path2 = "C:\\Users\\Lenovo\\Desktop\\test2.txt";
+		try {
+			FileInputStream fis = new FileInputStream(new File(path1));
+			FileOutputStream fos = new FileOutputStream(new File(path2));
+			byte[] tab_bytes = new byte[1];
+			
+			while(fis.read(tab_bytes)>0)
+			{
+				fos.write(tab_bytes);
+			}
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 
 	}
